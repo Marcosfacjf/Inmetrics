fdd628bdd34a5b0157a76bba2e9429dfec85d134
marcosfacjf <marcos.cunha@squadra.com.br>
2020-08-13 22:46:22 -0300

    Projeto Teste

---
+++ ProjetoAPI/InmetricsRest/src/main/java/br/ce/inmetrics/rest/core/Constantes.java
@@ -0,0 +1,14 @@
+package br.ce.inmetrics.rest.core;
+
+import io.restassured.http.ContentType;
+
+public interface Constantes {
+	
+	String APP_BASE_URL = "https://inmetrics:automacao@inm-api-test.herokuapp.com/";
+	Integer APP_PORT = 443;
+	String APP_BASE_PATH = "";
+	
+	ContentType APP_CONTENT_TYPE = ContentType.JSON;
+	
+	Long MAX_TIMEOUT = 10000L;
+	}
